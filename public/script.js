7607f877b94986948884bb7f3c5ac93359bd7b0f
Mian Hamid Ur Rehman <148252210+Hamid6426@users.noreply.github.com>
2024-07-05 17:08:54 +0500

    Update script.js
    Chore: Adding comments

--- public/script.js
+++ public/script.js
@@ -1,17 +1,20 @@
+// Function for fetching data from API endpoint
 async function fetchData() {
     try {
         const response = await fetch('/api/data');
         const data = await response.json();
         displayData(data);
     } catch (error) {
-        document.getElementById('api-data').innerText = 'Error fetching data';
+        document.getElementById('api-data').innerText = 'Error: fetching data';
         console.error(error);
     }
 }
 
+// Function for displaying the data fetched from API endpoint
 function displayData(data) {
     const apiDataDiv = document.getElementById('api-data');
     apiDataDiv.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
 }
 
+// Call the function
 fetchData();
